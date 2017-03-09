6d8b60b8219698dd9b5e6c5fa9e65603973f298a
Puneethnaik <puneethn250@gmail.com>
2017-03-09 07:14:18 +0530

    My first pygame interction!!!

---
+++ tut1.py
@@ -0,0 +1,11 @@
+import pygame
+from pygame.locals import *
+screen = pygame.display.set_mode([1024, 728])
+#car = pygame.image.load("/home/puneeth/Desktop/googledp.jpg")
+#screen.blit(car, (50, 100))
+pygame.display.flip()
+running = True # this indicates the state of the program
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False # this is means the user has pressed the close button on the window
